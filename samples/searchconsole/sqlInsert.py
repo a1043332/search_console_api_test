ca53b2fc80f5225e974d25e17c1dcb1d8fff93f7
David <a1043332@mail,nuk,edu,tw>
2020-09-08 10:16:54 +0000

    rrrrrrfx

---
+++ samples/searchconsole/sqlInsert.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error
+
+def sqlInsert(sql):
+    try:
+        
+        connection = mysql.connector.connect(
+            host='localhost',         
+            database='SC', 
+            user='hdd',        
+            password='hdd')  
+
+        if connection.is_connected():
+
+            
+            db_Info = connection.get_server_info()
+            print("version", db_Info)
+
+            
+
+            cursor = connection.cursor()
+            cursor.execute(sql)
+            connection.commit()
+            
+
+    except Error as e:
+        print("error", e)
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("closed") 
